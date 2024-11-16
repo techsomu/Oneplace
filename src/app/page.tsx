e36204684522cc681349873a69bd72a440b9ec8e
Suma2310 <sumasrikada@gmail.com>
2024-11-16 14:09:01 +0530

    autoclose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,15 @@
-
 import { Hero } from "@/components/hero";
 import MenuBar from "@/pages/Menubar";
 import React from "react";
 
 const Home: React.FC = () => {
-
-
   return (
     <>
-      <div className="headerdiv">
-
-      </div>
+      <div className="headerdiv"></div>
       <MenuBar />
       <Hero />
-     
-       </>
-      
-    
-     
-    
-
-    
-      
+    </>
   );
 };
 
 export default Home;
-
-
